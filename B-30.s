f0c7c253e5330d6403fc8f0be352697c368cc968
Lucas Natario <lucasenatario@gmail.com>
2024-04-21 14:04:57 -0300

    B30

---
+++ B-30.s
@@ -0,0 +1,100 @@
+# B.30 Transforme el ejercicio B.28 en una subrutina llamada PushMax, la cual reciba en a0 la
+# dirección de comienzo del vector y en a1 la cantidad de elementos del mismo. Cuando la
+# subrutina retorna debe haber “empujado” el elemento mayor a la última posición del vector.
+# Verifique luego del retorno llamando a PrintVector.
+
+.data
+    vector: .word 100,-20,40,-365,400,65536,1,0,3,-10
+.text
+    la a0, vector
+    li a1, 10 # cant elems
+    jal x1, PrintVector
+
+    la a0, vector
+    li a1,10 # cant elems    
+    jal x1, PushMax
+    
+    #newline
+    li a7, 11
+    li a0, 10
+    ecall
+    
+    la a0, vector
+    li a1, 10 # cant elems
+    jal x1, PrintVector
+ 
+     jal x1, fin
+PushMax:
+    add s9, x0, x1 # backup return register    
+    
+    add s10, x0, a0 # dir del vector
+    add s11, x0, a1 # contador de iteraciones
+    addi s11, s11, -1
+    
+    iterar_vector:
+    beq s11, x0, fin_iteracion
+    
+    add a0, x0, s10 #arg1 MaxDir - Elem actual
+    addi a1, a0, 4 #arg2 MaxDir - Elem sig
+    jal x1, MaxDir
+    
+    bne a0, x0, do_swap
+    beq a0, x0, continuar
+    do_swap:
+        add a0, x0, s10 #arg1 Swap - dir Elem actual
+        addi a1, a0, 4  #arg2 Swap - dir Elem sig
+        jal x1, Swap
+     
+    continuar:
+    addi s11, s11, -1 # contador iteraciones
+    addi s10, s10, 4 # muevo un word
+    jal t2, iterar_vector
+fin_iteracion:
+    jalr x0, s9, 0
+
+fin:
+    li a7, 10
+    ecall
+    
+PrintVector: # recibo en a0 el inicio del vector y en a1 cant de elementos
+    add t1, x0, a0 # bkp inicio vector
+    PrintVector_Iterar:
+    beq a1, x0, PrintVector_Fin
+    
+    addi a7, x0, 1 # printInt
+    lw a0, 0(t1) # guardo elem posicion
+    ecall
+    
+    addi a7, x0, 11 # printChar
+    addi a0, x0, 44 # comma
+    ecall
+    
+    addi a1, a1, -1
+    addi t1, t1, 4 # muevo un word
+    jal t2, PrintVector_Iterar
+    PrintVector_Fin:
+    jalr x0, x1, 0
+
+MaxDir: # recibe en a0 y en a1 dos direcciones, deja 1 en a0 si a0 es mayor
+    lw a0, 0(a0) # cargo valores apuntados para pasar a max
+    lw a1, 0(a1) # cargo valores apuntados para pasar a max
+    add t4, x0, x1 # bkp return reg
+    jal x1, Max
+    jalr x0, t4, 0
+    
+Max:
+    bge a1,a0,a1_mayor
+    bge a0, a1,a0_mayor
+    a1_mayor:
+        addi a0, x0, 0
+        jalr x0, x1, 0
+    a0_mayor:
+        addi a0, x0, 1
+        jalr x0, x1, 0
+Swap:
+    lw t0, 0(a0) # guardo valor de a0
+    lw t1, 0(a1) # guardo valor de a1
+    
+    sw t0, 0(a1) # pongo valor de a0 en a1
+    sw t1, 0(a0) # pongo valor de a1 en a0
+    jalr x0, x1, 0
